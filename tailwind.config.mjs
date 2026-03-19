c780e03fc5c1535bbd71ce306da2eb71dee96f23
lucaandreula11-commits <lucaandreula11@gmail.com>
2026-03-19 17:19:30 +0100

    Create tailwind.config.mjs

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#050505",
+        accent: "#3b82f6",
+      },
+    },
+  },
+  plugins: [],
+};
