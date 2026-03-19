94d246ba3c34a1f848bd31e1a8675c662378b429
lucaandreula11-commits <lucaandreula11@gmail.com>
2026-03-19 17:53:35 +0100

    Update tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,12 +2,12 @@
 export default {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./lib/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        dark: "#050505",
         accent: "#3b82f6",
       },
     },
