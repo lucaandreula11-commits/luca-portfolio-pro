9555337e4079afff2faf5ad8b044f1a78c45cfc2
lucaandreula11-commits <lucaandreula11@gmail.com>
2026-03-19 17:55:13 +0100

    Create next-env.d.ts

---
+++ next-env.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="next" />
+/// <reference types="next/navigation-types/compat/navigation" />
+
+// Questo file assicura che TypeScript riconosca i tipi di Next.js.
+// Non modificarlo manualmente, verrà aggiornato automaticamente da Next.js durante la build.
